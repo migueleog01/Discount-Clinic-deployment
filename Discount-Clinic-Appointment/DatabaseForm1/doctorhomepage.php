771b9c96268b99b31c6e1da1f2806c0a2ef2118e
rehmanessanidev <72720538+rehmanessanidev@users.noreply.github.com>
2023-03-25 17:59:18 -0500

    updated doctor home page

--- Discount-Clinic-Appointment/DatabaseForm1/doctorhomepage.php
+++ Discount-Clinic-Appointment/DatabaseForm1/doctorhomepage.php
@@ -11,14 +11,22 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-	<title>My website</title>
+	<title>Medical Clinic Home Page</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
+	<header>
+		<h1><center>Discount Clinic</center></h1>
+		<nav>
+			<ul>
+				<li class ="active"><a href="doctorhomepage.php">Home</a></li>
+				<li><a href="doctorappointments.php">Appointments</a></li>
+                <li><a href="./doctorprofile.php">Profile</a></li>
+			</ul>
+		</nav>
+	</header>
+    <main>
+		<h2><center>Hello, <?php echo $user_data['username']; ?></center></h2>
 
-	<a href="logout.php">Logout</a>
-	<h1>Home page</h1>
-
-	<br>
-	Hello, <?php echo $user_data['username']; ?>
 </body>
 </html>
