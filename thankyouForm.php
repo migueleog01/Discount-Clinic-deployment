71b203adaae26655bb0239fc643b015d34f3a8fd
lllacoxlll <65261676+lllacoxlll@users.noreply.github.com>
2023-03-25 07:08:38 -0500

    Add files via upload

---
+++ thankyouForm.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
+<h1>Thank you for submitting the form!</h1>
+<p>Your data has been saved.</p>
+</body>
+
+</html>
+
+
+
+
+<?php 
+    
+
+
+
+
+?>
