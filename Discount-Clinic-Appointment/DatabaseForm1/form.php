93f7e5178ec7de81f912379da72cdf870b2e71db
lllacoxlll <65261676+lllacoxlll@users.noreply.github.com>
2023-03-25 20:14:22 -0500

    Update form.php

--- Discount-Clinic-Appointment/DatabaseForm1/form.php
+++ Discount-Clinic-Appointment/DatabaseForm1/form.php
@@ -83,28 +83,6 @@ session_start();
         $allergies = $_POST['allergies'];
 
         $emergencyMiddleInitial =  'D';
-        // Create a new MySQLi connection
-        // include("dbh-inc.php");
-
-
-        /*
-        $servername = "discountclinic.mysql.database.azure.com";
-        $username = "adminLogin";
-        $password = "1234567c!";
-        $dbname = "discount_clinic";
-
-        // Create connection
-        $conn = mysqli_init();
-        mysqli_ssl_set($conn,NULL,NULL,"DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-        mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, MYSQLI_CLIENT_SSL);
-
-        // Check connection
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
-        */
-
-
 
 
         // Insert values into friends table
