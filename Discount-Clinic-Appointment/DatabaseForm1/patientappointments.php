5faa7ef9c4a4eab227b8e489b1db7346ba5a3f3c
lllacoxlll <65261676+lllacoxlll@users.noreply.github.com>
2023-03-25 23:20:17 -0500

    Create patientappointments.php

--- Discount-Clinic-Appointment/DatabaseForm1/patientappointments.php
+++ Discount-Clinic-Appointment/DatabaseForm1/patientappointments.php
@@ -1,185 +1,42 @@
-<!DOCTYPE html>
-<html>
-    <header>
-        <div class="logo">
-          <h1>Discount Clinic</h1>
-        </div>
-        <nav>
-          <ul>
-            <li><a href="patienthomepage.php">Home</a></li>
-            <li><a href="appointments.html">Appointments</a></li>
-            <li><a href="transactions.html">Transactions</a></li>
-            <li><a href="profile.html">Profile</a></li>
-          </ul>
-        </nav>
-      </header>
-<head>
-	<title>Appointment Making System</title>
-	<link rel="stylesheet" href="style.css">
-</head>
-<body>
+const dateInput = document.getElementById("date");
+const errorMessage = document.getElementById("error-message");
 
-	<div class="container">
-		<h2>Appointment Form</h2>
-		<form action="#" method="POST">
-			
-
-			<label for="email">Email:</label>
-			<input type="email" id="email" name="email" required>
-
-			<label for="phone">Phone:</label>
-			<input type="text" id="phone" name="phone" required>
-
-			<label for="date">Date:</label>
-			<input type="date" id="date" name="date" required>
-
-			<label for="time">Time:</label>
-            <select id="time" name="time" required></select>
-
-			<label for="primary-physician">Primary Physician:</label>
-
-			<label for="physician1"><input type="radio" id="physician1" name="primary-physician" value="302" required>Dr. Miguel</label>
-			<!-- make an if statent, if doctor id is physician1, display adress 158 Easton Glen Ln and give it a value of 1011 -->
-			
+dateInput.addEventListener('input', function() {
+  const enteredDate = dateInput.value;
+  const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
+  if (!dateRegex.test(enteredDate)) {
+    errorMessage.textContent = "Please enter a valid date in the format MM/DD/YYYY";
+  } else {
+    errorMessage.textContent = "";
+  }
+});
 
 
-			<label for="physician2"><input type="radio" id="physician2" name="primary-physician" value="305" required>Dr. Doctor</label>
+  // Add code here to send data to server, save to database, or perform any other necessary action
 
-			<label for="physician3"><input type="radio" id="physician3" name="primary-physician" value="Dr. Bob Johnson" required>Dr. Bob Johnson</label>
-
-			
-
-			<button type="submit" value = "Submit" id="submitBtn">Submit</button>
-		</form>
-	</div>
-    
-	<script src="script.js"></script>
-</body>
-</html>
+ 
 
 
-<?php
-	
-	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-		$date = $_POST['date'];
-		$date = date('Y-m-d', strtotime($date));
-		$time = $_POST['time'];
-		$time = date('H:i', strtotime($time));
-		$physician = $_POST['primary-physician'];
-		//$email = $_POST['email'];
-		//$phone = $_POST['phone'];\
-		//echo $date;
-		//echo $time;
-		//echo $physician;
-		//make an if statemtn if physician is 302 the adress id is 1010
-
-		if($physician == 302){
-			$address = 1010;
-		}
-		if($physician == 305){
-			$address = 1011;
-		}
-		
-
-		$servername = "discountclinic.mysql.database.azure.com";
-        $username = "adminLogin";
-        $password = "1234567c!";
-        $dbname = "discount_clinic";
-		
-        // Create connection
-        $conn = mysqli_init();
-        mysqli_ssl_set($conn,NULL,NULL,"DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-        mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, MYSQLI_CLIENT_SSL);
-
-        // Check connection
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
-		//from our database if doctor specialty attrubute is is not primary, then set a variable 0 in php 
-		
-		$test = false;
-
-		$sql_doctor = "SELECT * FROM doctor WHERE doctor_id = '$physician' AND specialty = 'primary'";
-		//echo $sql_doctor;
-		$result = mysqli_query($conn, $sql_doctor);
-		if ($result && mysqli_num_rows($result) > 0) {
-			// Specialty is primary for the given physician
-			//$sql = "INSERT INTO appointment (patient_id, date, time, doctor_id,office_id,deleted) VALUES (1 ,'$date', '$time', '$physician', '$address', '0')";
-			echo "appointment deleted must be 0";
-			$test = true;
-			//echo "true";
-		} else {
-			//$sql = "INSERT INTO appointment (patient_id, date, time, doctor_id,office_id) VALUES (1 ,'$date', '$time', '$physician', '$address')";
-			// Specialty is not primary for the given physician
-			echo "appointment deleted must be 1";
-			$test = false;
-		}
-		//if true delete must be 0
-		//if false delete must be 1
+var startHour = 8; // 8am
+var endHour = 14; // 2pm
+var select = document.getElementById("time");
+for (var hour = startHour; hour < endHour; hour++) {
+    for (var minute = 0; minute < 60; minute += 30) {
+        var time = new Date();
+        time.setHours(hour);
+        time.setMinutes(minute);
+        var option = document.createElement("option");
+        option.value = time.toLocaleTimeString('en-US', {hour: 'numeric', minute: 'numeric'});
+        option.text = time.toLocaleTimeString('en-US', {hour: 'numeric', minute: 'numeric'});
+        select.add(option);
+    }
+}
 
 
-		if($test === true ){
-			echo "true";
-			$sql = "INSERT INTO appointment (patient_id, date, time, doctor_id,office_id,deleted) VALUES (1 ,'$date', '$time', '$physician', '$address', 0)";
-			if (mysqli_query($conn, $sql)) {
-				echo "New record created successfully";
-			} else {
-				echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-			}
-		}
-		else{
-			//echo "YOU NEED APPROVAL FROM SPECIALIST";
-			echo "<form action='approvalHasBeenSent.php' method='post'>";
-    		echo "<input type='submit' value='Request Approval'>";
-    		echo "</form>";
-			
-			//$sql = "INSERT INTO appointment (patient_id, date, time, doctor_id,office_id,deleted) VALUES (1 ,'$date', '$time', '$physician', '$address',1)";
-			
-		}
+const doctorId = document.getElementById("physician1").value;
+const addressElement = document.getElementById("address");
 
-
-
-
-		/*
-		if( $sql_doctor != 0 )
-		{
-			$sql = "INSERT INTO appointment (patient_id, date, time, doctor_id,office_id,deleted) VALUES (1 ,'$date', '$time', '$physician', '$address', '0')";
-			if(mysqli_query($conn, $sql)){
-				echo "Records added successfully.";
-				exit(1);
-			}
-		}
-		else{
-			$sql = "INSERT INTO appointment (patient_id, date, time, doctor_id,office_id) VALUES (1 ,'$date', '$time', '$physician', '$address')";
-
-		}
-		*/
-		
-		
-		//echo $sql_doctor;
-
-
-		
-		
-
-		// Close connection
-		mysqli_close($conn);
-		
-		
-		//$sql = "INSERT INTO appointment (patient_id, date, time, doctor_id,office_id,deleted) VALUES (1 ,'$date', '$time', '$physician', '$address', '0')";
-		
-	
-	
-	
-	}
-		
-
-
-
-
-
-
-	
-
-
-?>
+if (doctorId === "physician1") {
+  addressElement.value = "158 Easton Glen Ln";
+  addressElement.setAttribute("value", "1011");
+}
