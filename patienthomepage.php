5eecd2ba44e88ea8c8b288d8e3af91b97112de51
migueleog01 <99515060+migueleog01@users.noreply.github.com>
2023-03-29 01:19:48 -0500

    Update patienthomepage.php

--- patienthomepage.php
+++ patienthomepage.php
@@ -7,6 +7,55 @@ session_start();
 	include("functions.php");
 
 	$user_data = check_login($conn);
+
+
+
+		
+
+		$TEST = $user_data['username'];
+		$query = "SELECT user_id FROM user WHERE username = '$TEST'";
+		$result = mysqli_query($conn, $query);
+		if($result && mysqli_num_rows($result) > 0) {
+			$user_data = mysqli_fetch_assoc($result);
+			$user_id = $user_data['user_id'];
+		} 
+
+
+
+		$query = "SELECT patient_id FROM patient WHERE user_id = '$user_id'";
+		$result = mysqli_query($conn, $query);
+
+		if($result && mysqli_num_rows($result) > 0) {
+			$patient_data = mysqli_fetch_assoc($result);
+			$patient_id = $patient_data['patient_id'];
+		}
+		
+
+		$sql = "SELECT * FROM appointment WHERE patient_id = '$patient_id' AND deleted = FALSE";
+
+
+		$sql = "SELECT * 
+		FROM discount_clinic.appointment, discount_clinic.office, discount_clinic.address, discount_clinic.doctor
+		WHERE patient_id = '$patient_id' AND office.address_id = address.address_id AND appointment.office_id = office.office_id AND appointment.doctor_id = doctor.doctor_id";
+
+		$result = $conn->query($sql);
+
+		if ($result->num_rows > 0) {
+			while ($row = $result->fetch_assoc()) {
+				echo "<tr>";
+				echo "<td>" . $row['appointment_id'] . "</td>";
+				echo "<td>" . $row['first_name'] . " " . $row['last_name'] ."</td>";
+				echo "<td>" . $row['date'] . "</td>";
+				echo "<td>" . $row['time'] . "</td>";
+				echo "<td>" . $row['street_address'] . " " . $row['city'] . " " . $row['state'] . " " . $row['zip'] . "</td>";
+				echo "</tr>";
+			}
+		} else {
+			echo "<tr><td colspan='5'>No appointments found.</td></tr>";
+		}
+		
+		$conn->close();
+		ob_flush();
 ?>
 
 <!DOCTYPE html>
@@ -81,54 +130,7 @@ session_start();
     </tr>
   </thead>
   <tbody>
-	<?php
-	  	ob_start();
-		
-
-		$TEST = $user_data['username'];
-		$query = "SELECT user_id FROM user WHERE username = '$TEST'";
-		$result = mysqli_query($conn, $query);
-		if($result && mysqli_num_rows($result) > 0) {
-			$user_data = mysqli_fetch_assoc($result);
-			$user_id = $user_data['user_id'];
-		} 
-
-
-
-		$query = "SELECT patient_id FROM patient WHERE user_id = '$user_id'";
-		$result = mysqli_query($conn, $query);
-
-		if($result && mysqli_num_rows($result) > 0) {
-			$patient_data = mysqli_fetch_assoc($result);
-			$patient_id = $patient_data['patient_id'];
-		}
-		
-
-		$sql = "SELECT * FROM appointment WHERE patient_id = '$patient_id' AND deleted = FALSE";
-
-
-		$sql = "SELECT * 
-		FROM discount_clinic.appointment, discount_clinic.office, discount_clinic.address, discount_clinic.doctor
-		WHERE patient_id = '$patient_id' AND office.address_id = address.address_id AND appointment.office_id = office.office_id AND appointment.doctor_id = doctor.doctor_id";
-
-		$result = $conn->query($sql);
-
-		if ($result->num_rows > 0) {
-			while ($row = $result->fetch_assoc()) {
-				echo "<tr>";
-				echo "<td>" . $row['appointment_id'] . "</td>";
-				echo "<td>" . $row['first_name'] . " " . $row['last_name'] ."</td>";
-				echo "<td>" . $row['date'] . "</td>";
-				echo "<td>" . $row['time'] . "</td>";
-				echo "<td>" . $row['street_address'] . " " . $row['city'] . " " . $row['state'] . " " . $row['zip'] . "</td>";
-				echo "</tr>";
-			}
-		} else {
-			echo "<tr><td colspan='5'>No appointments found.</td></tr>";
-		}
-		
-		$conn->close();
-	?>
+	
   </tbody>
 </table>
 </body>
