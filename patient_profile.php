a0c090485ea41109cf902e42b06835daedebdbf6
migueleog01 <99515060+migueleog01@users.noreply.github.com>
2023-03-29 02:08:04 -0500

    Update patient_profile.php

--- patient_profile.php
+++ patient_profile.php
@@ -4,54 +4,111 @@
     include("functions.php");
 
     $user_data = check_login($conn);
-    $user_id_fk = $user_data['user_ID'];
+    $user_id = $user_data['user_ID'];
 
-    $patient_name = "SELECT first_name FROM patient WHERE user_id = '$user_id_fk'";
+    $patient = "SELECT first_name, middle_initial, last_name, gender, patient.phone_number AS patient_phone_number, DOB, total_owe, e_first_name, e_middle_initial, e_last_name, emergency_contact.phone_number AS ec_phone_number, relationship, street_address, zip, state, city
+	FROM discount_clinic.patient, discount_clinic.emergency_contact, discount_clinic.user, discount_clinic.address
+	WHERE patient.patient_id = emergency_contact.patient_id AND user.user_id = patient.user_id AND patient.address_id = address.address_id AND user.user_id = '$user_id'";
+	 
+   	
 
-    $first_name = mysqli_query($conn, $patient_name);
+    $patient_result = mysqli_query($conn, $patient);
 
-		if($first_name && mysqli_num_rows($first_name) > 0)
-		{
+	if($patient_result && mysqli_num_rows($patient_result) > 0) {
+		$user_data = mysqli_fetch_assoc($patient_result);
+		$user_first_name = $user_data['first_name'];
+		$user_middle_initial =  $user_data['middle_initial'];
+		$user_last_name =  $user_data['last_name'];
+		$gender = $user_data['gender'];
+		$phone_number = $user_data['patient_phone_number'];
+		$DOB = $user_data['DOB'];
+		$total_owe = $user_data['total_owe'];
 
-			$name = mysqli_fetch_assoc($first_name);
-			$output = $name['first_name'];
-			echo $output;
 
-		}
+		$street_address = $user_data['street_address'];
+		$city = $user_data['city'];
+		$state = $user_data['state'];
+		$zip = $user_data['zip'];
+
+
+		$e_first_name = $user_data['e_first_name'];
+		$e_middle_initial = $user_data['e_middle_initial'];
+		$e_last_name = $user_data['e_last_name'];
+		$e_phone_number = $user_data['ec_phone_number'];
+		$relationship = $user_data['relationship'];
+	} 
+
+
+	$output = $row['first_name'];
+	$output = $row['last_name'];
+	echo $output;
+	
 ?>
 
 
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="styles.css">
 	<title>Patient Profile</title>
+	<header>
+		<h1><center>Patient Profile</center></h1>
+		<nav>
+			<ul>
+				<li class ="active"><a href="index.php">Home</a></li>
+				<li><a href="patient_profile.php">Profile</a></li>
+				<li><a href="patientappointments.php">Schedule Appointment</a></li>
+        		<li><a href="transactions.php">Transactions</a></li>
+				<li><a href ="form.php">Patient Form</a></li>
+				<li><a href="logout.php">Logout</a></li>
+			</ul>
+		</nav>
+	</header>
 	<style>
-		h1 {
-			font-size: 40px;
-			color: #333;
-			margin-top: 30px;
-			margin-bottom: 20px;
-            background-color: #192841; 
-		}
-		p {
-			font-size: 16px;
-			color: #555;
-			margin-bottom: 10px;
-		}
-	</style>
+	table {
+		border-collapse: collapse;
+		width: 100%;
+	}
+
+	th, td {
+		text-align: center;
+		padding: 8px;
+		border: 1px solid #ddd;
+	}
+
+	tr:nth-child(even) {
+		background-color: #f2f2f2;
+	}
+
+	h1 {
+		font-size: 50px;
+		margin-bottom: 20px;
+	}
+
+	.container {
+		margin: auto;
+		max-width: 800px;
+		padding: 20px;
+	}
+</style>
 </head>
 <body>
-	<h1><center>Patient Profile</center></h1>
-	<p><strong>Patient Name:</strong> John Doe</p>
-    <p><strong>Gender:</strong> Male</p>
-	<p><strong>Date of Birth:</strong> January 1, 1980</p>
-	<p><strong>Address:</strong> 123 Main Street, Anytown, USA</p>
-	<p><strong>Phone:</strong> 555-1234</p>
-	<p><strong>Email:</strong> john.doe@email.com</p>
-	<p><strong>Current Primary Doctor:</strong> Dr. Patel</p>
-    <p><strong>Approval To See Specialist:</strong> No </p>
-    <p><strong>Medical Conditions:</strong> Diabetes, High Blood Pressure</p>
-	<p><strong>Allergies:</strong> Peanuts, Shellfish</p>
-</body>
+	<h2>Personal Information</h2>
+	<?php 
 
+		echo "Name: " . $user_first_name . " " . $user_middle_initial . " " . $user_last_name . "<br>";
+		echo "Address: " . $street_address . " " . $city . ", " . $state . " " . $zip . "<br>";
+		echo "Gender: " . $gender . "<br>";
+		echo "Date of Birth: " . $DOB . "<br>";
+		echo "Phone Number: " . $phone_number . "<br>";
+
+	 ?>
+	 <h2>Emergency Contact Information</h2>
+	 <?php 
+
+	 	echo "Name: " . $e_first_name . " " . $e_middle_initial . " " . $e_last_name . "<br>";
+	 	echo "Phone Number: " . $e_phone_number . "<br>";
+	 	echo "Relationship: " . $relationship . "<br>";
+	  ?>
+</body>
 </html>
