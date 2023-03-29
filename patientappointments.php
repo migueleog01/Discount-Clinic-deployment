14649420a3c153b8e349c2034c267fb9acc2a7ed
migueleog01 <99515060+migueleog01@users.noreply.github.com>
2023-03-29 03:18:18 -0500

    Update patientappointments.php

--- patientappointments.php
+++ patientappointments.php
@@ -58,7 +58,8 @@ $result = mysqli_query($conn, $sql);
 <!-- $sql = "SELECT * 
 FROM discount_clinic.doctor_office, discount_clinic.office, discount_clinic.doctor, discount_clinic.address
 WHERE office.office_id = doctor_office.OID AND doctor.doctor_id = doctor_office.DID AND address.address_id = office.address_id;"; -->
-
+<script src="patient_appointments_script.js" defer></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script>
   let rows = [];
   let row = [];
@@ -69,7 +70,7 @@ WHERE office.office_id = doctor_office.OID AND doctor.doctor_id = doctor_office.
       $fname = $row["first_name"];
       $specialty = $row["specialty"];
   ?>
-  row = ['<?php echo $address; ?>', '<?php echo $fname; ?>', '<?php echo $specialty ?>'];
+  row = ['<?php echo $address; ?>', '<?php echo $fname; ?>', '<?php echo $specialty; ?>'];
   rows.push(row);
   row = [];
   // rows.push(<?php echo $row;?>)
@@ -91,7 +92,8 @@ WHERE office.office_id = doctor_office.OID AND doctor.doctor_id = doctor_office.
 			<input type="date" id="date" name="date" required>
 
 			<label for="time">Time:</label>
-            <select id="time" name="time" required></select>
+            <select id="time" name="time" required>
+            <option value=""></option></select>
 		
 
 
@@ -195,6 +197,6 @@ WHERE office.office_id = doctor_office.OID AND doctor.doctor_id = doctor_office.
 		</form>
 	</div>
     
-	<script src="patient_appointments_script.js"></script>
+	
 </body>
 </html>
