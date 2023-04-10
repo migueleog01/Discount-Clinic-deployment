bd2cbcc10119484d73477c901e1407b351aa44d6
Miguel Garcia <miguelgarcia@Miguels-MacBook-Pro-6.local>
2023-04-10 14:01:22 -0500

    modified index and added new patient form

--- index.php
+++ index.php
@@ -10,8 +10,23 @@ ob_start();
 	$user_data = check_login($conn);
 
 	// if the user is a patient (has role=patient) then this page should have a link to the registration form
-	if($user_data['role']==='patient')
-		header("Location: patienthomepage.php");
+	if($user_data['role']==='patient'){
+		$TEST = $user_data['username'];
+		$query = "SELECT user_ID FROM user WHERE username = '$TEST'";
+		$result = mysqli_query($conn, $query);
+		if($result && mysqli_num_rows($result) > 0) {
+			$user_data = mysqli_fetch_assoc($result);
+			$user_id = $user_data['user_ID'];
+		}
+		$patient_exists = "SELECT patient.user_id FROM patient,user WHERE patient.user_id='$user_id'";
+		$res = mysqli_query($conn, $patient_exists);
+		if($res && mysqli_num_rows($res)>0){
+			header("Location: patienthomepage.php");
+		}
+		else{
+			header("Location: new_patient_form.php");
+		}
+	}
 	elseif($user_data['role']==='doctor')
 		header("Location: doctorhomepage.php");
 ?>
