4290dd711993eed73dbc5cf2ee989d207fc6d7af
Miguel Garcia <miguelgarcia@Miguels-MacBook-Pro-6.local>
2023-04-10 17:33:35 -0500

    ADDED approval to search bar

--- doctor_profile.php
+++ doctor_profile.php
@@ -49,6 +49,7 @@ $result = $conn->query($sql);
                 <li class="active"><a href="doctorhomepage.php">Home</a></li>
                 <li><a href="doctor_profile.php">Profile</a></li>
                 <li><a href="doctorappointments.php">Appointments</a></li>
+                <li><a href="approval.php">Approval</a></li>
                 <li><a href="logout.php">Logout</a></li>
             </ul>
         </nav>
