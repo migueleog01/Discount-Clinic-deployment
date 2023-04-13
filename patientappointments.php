5bdc95949b1e89046ca6b65986528afc50c80691
migueleog01 <99515060+migueleog01@users.noreply.github.com>
2023-04-13 01:54:36 -0500

    other helper commented out ob_start and in pattient appointment i left php in middle testing 1

--- patientappointments.php
+++ patientappointments.php
@@ -1,6 +1,3 @@
-
-
-
 <!DOCTYPE html>
 <html>
     <header>
@@ -19,8 +16,8 @@
       </header>
 <head>
 
-	<title>Appointment Making System</title>
-	<link rel="stylesheet" href="patient_appointments_style.css">
+  <title>Appointment Making System</title>
+  <link rel="stylesheet" href="patient_appointments_style.css">
 </head>
 
   <script src="patient_appointments_script.js" defer></script>
@@ -68,100 +65,6 @@
 </script>
 
 <body>
-
-
-
-
-	<div class="container">
-		<h2>Appointment Form</h2>
-		<form action="" method="POST">
-
-
-      <label for="date">Date:</label>
-      <input type="date" id="date" name="date" required>
-
-      <label for="time">Time:</label>
-            <select id="time" name="time" required>
-            <option value=""></option>
-            </select>
-
-
-        <label for="state">Select a State:</label>
-				<select id="state" name="state" onchange="my_fun(this.value);">
-
-            <option value=""></option>
-            <option value="AL">Alabama</option>
-            <option value="AK">Alaska</option>
-            <option value="AZ">Arizona</option>
-            <option value="AR">Arkansas</option>
-            <option value="CA">California</option>
-            <option value="CO">Colorado</option>
-            <option value="CT">Connecticut</option>
-            <option value="DE">Delaware</option>
-            <option value="DC">District Of Columbia</option>
-            <option value="FL">Florida</option>
-            <option value="GA">Georgia</option>
-            <option value="HI">Hawaii</option>
-            <option value="ID">Idaho</option>
-            <option value="IL">Illinois</option>
-            <option value="IN">Indiana</option>
-            <option value="IA">Iowa</option>
-            <option value="KS">Kansas</option>
-            <option value="KY">Kentucky</option>
-            <option value="LA">Louisiana</option>
-            <option value="ME">Maine</option>
-            <option value="MD">Maryland</option>
-            <option value="MA">Massachusetts</option>
-            <option value="MI">Michigan</option>
-            <option value="MN">Minnesota</option>
-            <option value="MS">Mississippi</option>
-            <option value="MO">Missouri</option>
-            <option value="MT">Montana</option>
-            <option value="NE">Nebraska</option>
-            <option value="NV">Nevada</option>
-            <option value="NH">New Hampshire</option>
-            <option value="NJ">New Jersey</option>
-            <option value="NM">New Mexico</option>
-            <option value="NY">New York</option>
-            <option value="NC">North Carolina</option>
-            <option value="ND">North Dakota</option>
-            <option value="OH">Ohio</option>
-            <option value="OK">Oklahoma</option>
-            <option value="OR">Oregon</option>
-            <option value="PA">Pennsylvania</option>
-            <option value="RI">Rhode Island</option>
-            <option value="SC">South Carolina</option>
-            <option value="SD">South Dakota</option>
-            <option value="TN">Tennessee</option>
-            <option value="TX">Texas</option>
-            <option value="UT">Utah</option>
-            <option value="VT">Vermont</option>
-            <option value="VA">Virginia</option>
-            <option value="WA">Washington</option>
-            <option value="WV">West Virginia</option>
-            <option value="WI">Wisconsin</option>
-            <option value="WY">Wyoming</option>
-        </select>
-
-          <label for="office">Select an Office:</label>
-					<select id="office" name="office" onchange="my_other_fun(this.value);">
-						<option value="">Select location</option>
-					</select>
-
-          <label for="doctor">Select a Doctor:</label>
-          <select id="doctor" name="doctor" required>
-            <option value="">Select doctor</option>
-          </select>
-
-          <div></div>
-
-
-			<button type="submit" value = "Submit" id="submitBtn">Submit</button>
-		</form>
-	</div>
-</body>
-</html>
-
 
 
 <?php
@@ -240,3 +143,93 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
 ?>
+
+  <div class="container">
+    <h2>Appointment Form</h2>
+    <form action="#" method="POST">
+
+
+      <label for="date">Date:</label>
+      <input type="date" id="date" name="date" required>
+
+      <label for="time">Time:</label>
+            <select id="time" name="time" required>
+            <option value=""></option>
+            </select>
+
+
+        <label for="state">Select a State:</label>
+        <select id="state" name="state" onchange="my_fun(this.value);">
+
+            <option value=""></option>
+            <option value="AL">Alabama</option>
+            <option value="AK">Alaska</option>
+            <option value="AZ">Arizona</option>
+            <option value="AR">Arkansas</option>
+            <option value="CA">California</option>
+            <option value="CO">Colorado</option>
+            <option value="CT">Connecticut</option>
+            <option value="DE">Delaware</option>
+            <option value="DC">District Of Columbia</option>
+            <option value="FL">Florida</option>
+            <option value="GA">Georgia</option>
+            <option value="HI">Hawaii</option>
+            <option value="ID">Idaho</option>
+            <option value="IL">Illinois</option>
+            <option value="IN">Indiana</option>
+            <option value="IA">Iowa</option>
+            <option value="KS">Kansas</option>
+            <option value="KY">Kentucky</option>
+            <option value="LA">Louisiana</option>
+            <option value="ME">Maine</option>
+            <option value="MD">Maryland</option>
+            <option value="MA">Massachusetts</option>
+            <option value="MI">Michigan</option>
+            <option value="MN">Minnesota</option>
+            <option value="MS">Mississippi</option>
+            <option value="MO">Missouri</option>
+            <option value="MT">Montana</option>
+            <option value="NE">Nebraska</option>
+            <option value="NV">Nevada</option>
+            <option value="NH">New Hampshire</option>
+            <option value="NJ">New Jersey</option>
+            <option value="NM">New Mexico</option>
+            <option value="NY">New York</option>
+            <option value="NC">North Carolina</option>
+            <option value="ND">North Dakota</option>
+            <option value="OH">Ohio</option>
+            <option value="OK">Oklahoma</option>
+            <option value="OR">Oregon</option>
+            <option value="PA">Pennsylvania</option>
+            <option value="RI">Rhode Island</option>
+            <option value="SC">South Carolina</option>
+            <option value="SD">South Dakota</option>
+            <option value="TN">Tennessee</option>
+            <option value="TX">Texas</option>
+            <option value="UT">Utah</option>
+            <option value="VT">Vermont</option>
+            <option value="VA">Virginia</option>
+            <option value="WA">Washington</option>
+            <option value="WV">West Virginia</option>
+            <option value="WI">Wisconsin</option>
+            <option value="WY">Wyoming</option>
+        </select>
+
+          <label for="office">Select an Office:</label>
+          <select id="office" name="office" onchange="my_other_fun(this.value);">
+            <option value="">Select location</option>
+          </select>
+
+          <label for="doctor">Select a Doctor:</label>
+          <select id="doctor" name="doctor" required>
+            <option value="">Select doctor</option>
+          </select>
+
+         
+
+
+      <button type="submit" value = "Submit" id="submitBtn">Submit</button>
+    </form>
+  </div>
+</body>
+</html>
