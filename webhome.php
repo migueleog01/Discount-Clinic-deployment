bd16180fc60ea2324b3e7bbac350622cc600cc6d
Miguel Garcia <miguelgarcia@Miguels-MacBook-Pro-6.local>
2023-04-20 02:56:46 -0500

    addeded homepage

---
+++ webhome.php
@@ -0,0 +1,155 @@
+<?php
+session_start();
+?>
+ 
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Discount Clinic</title>
+    <!-- <link rel = "stylesheet" href = "style.css"> -->
+    <style>
+        * {
+    margin: 0;
+    padding: 0;
+    font-family: optima;
+}
+
+.banner {
+    width: 100%;
+    height: 100vh;
+    background-image: linear-gradient(rgba(45, 64, 61, 0.75), rgba(35, 49, 49, 0.75)), url(logo.png);
+    background-size: cover;
+    background-position: center;
+}
+
+.navbar {
+    width: 100%;
+    margin: auto;
+    padding: 35px 0;
+    display: flex;
+    float: right;
+    align-items: center;
+    justify-content: space-between;
+    position: absolute;
+    right: -1100px;
+}
+
+.logo {
+    width: 120px;
+    cursor: pointer;
+}
+
+.navbar ul li {
+    list-style: none;
+    display: inline-block;
+    margin: 0 20px;
+    position: relative;
+}
+
+.navbar ul li a {
+    text-decoration: none;
+    color: #fff;
+    text-transform: uppercase;
+}
+
+.navbar ul li::after {
+    content: '';
+    width: 100%;
+    height: 3px;
+    background: #009688;
+    position: absolute;
+    left: 0;
+    bottom: -10px;
+}
+
+.navbar ul li:hover::after {
+    width: 100%;
+}
+
+.content {
+    width: 100%;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    text-align: center;
+    color: #fff;
+}
+
+.content h1 {
+    font-size: 70px;
+    margin-top: 80px;
+}
+
+.content p {
+    font-size: 20px auto;
+    font-weight: 100;
+}
+
+.button-link {
+    border: 2px solid #009688;
+    border-radius: 25px;
+    background: transparent;
+    padding: 15px 0;
+    color: #fff;
+    font-size: 15px;
+    font-weight: bold;
+    text-align: center;
+    cursor: pointer;
+    margin: 20px 10px;
+    text-decoration: none;
+    display: inline-block;
+    width: 200px;
+    position: relative;
+    overflow: hidden;
+}
+
+.button-link span {
+    background: #009688;
+    height: 100%;
+    width: 100%;
+    border-radius: 25px;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    z-index: -1;
+    transition: 0.5s
+}
+
+.button-link:hover span {
+    width: 0%;
+}
+
+.button-link:hover {
+    color: #fff;
+    border: none;
+}
+
+    </style>
+</head>
+<body>
+    <div class = "banner">  
+        <div class = "navbar">  
+            <!--<img src = "randomfordb.jpeg" class = "logo">-->
+            <ul>
+                <li><a href = "#" >Home</a></li>
+                <li><a href = "#" >About</a></li>
+                <li><a href = "#" >Contact</a></li>
+        
+            </ul>
+        </div>
+
+        <div class = "content">
+            <h1>DISCOUNT CLINIC</h1>
+            <p>High quality appointments at a low cost!</p>
+            <div>   
+            <div>
+        <a href="login.php" class="button-link"><span></span>LOGIN</a>
+        <a href="register.php" class="button-link"><span></span>REGISTER</a>
+    </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
