97e03842bd754e4c988e85ab13cbdd014800aa59
migueleog01 <mlgarc25@cougarnet.uh.edu>
2023-04-23 18:15:39 -0500

    fixes status on the patient home page

--- patienthomepage.php
+++ patienthomepage.php
@@ -155,7 +155,7 @@ if ($result->num_rows > 0) {
         echo '<td>' . "approved" . '</td>';
       }
     } else {
-      echo '<td>' . "      " . '</td>';
+      echo '<td>' . "approved" . '</td>';
     }
 
     echo "<td>";
